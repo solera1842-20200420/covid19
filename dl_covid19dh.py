9784e0cc28aab968983f5d28490a2b7bca82493d
solera1842_202004 <63997418+solera1842-20200420@users.noreply.github.com>
2021-08-08 20:37:32 +0900

    Add files via upload

--- dl_covid19dh.py
+++ dl_covid19dh.py
@@ -1,18 +1,18 @@
 # covid19 data hubからデータのダウンロード
 # ライブラリのインポート
 from covid19dh import covid19
-import pandas as pd
+import os
+import pandas as pdb
 
 # x, src = covid19()
 # x, src = covid19("USA") # Unites States
 x, src = covid19("JPN") # Jaapan
 
 # データ保存先ディレクトリ
-data_dir = './data/'
+DATA_DIR = './data/'
 
 # csv書き込み
 print('covid19_data_hubの日本のデータ書き込み')
-x.to_csv(data_dir + 'covid19dh_jpn.csv', index=False)
-print('covid19_data_hubからの日本のデータの情報園の書き込み')
-src.to_csv(data_dir + 'covid19dh_jpn_src.csv', index=False)
-
+x.to_csv(os.path.join(DATA_DIR, 'covid19dh_jpn.csv'))
+print('covid19_data_hubからの日本のデータの情報源の書き込み')
+src.to_csv(os.path.join(DATA_DIR, 'covid19dh_jpn_src.csv'))
